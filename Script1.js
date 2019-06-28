a421c073373a0ef12068f31238f242f8e8e35c32
Bryon Gloden, CISSP® <cissp@bryongloden.com>
2019-06-27 21:36:52 -0400

    Initial Commit

---
+++ Script1.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+
+//Extract a value from key=value type strings
+function extract(str, key) {
+	if (str == null) return '';
+	var keyStart = str.indexOf(key + "=");
+	if (-1 === keyStart) return '';
+	var valStart = 1 + str.indexOf("=", keyStart);
+	var valEnd = str.indexOf("&", keyStart);
+	var val = -1 === valEnd ? str.substring(valStart) : str.substring(valStart, valEnd);
+	return val;
+}
+
+//Set the user's favorite background color
+function doColor() {
+	var h = location.hash.substring(1);
+	if (h.indexOf("faveColor=") >= 0) {
+		document.cookie = h; // Defect here
+	}
+	var faveColor = extract(document.cookie, "faveColor");
+	document.bgColor = faveColor;
+
+	console.log(document.cookie);
+}
+window.onhashchange = doColor;
